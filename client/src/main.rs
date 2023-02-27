4c2c52eb530d1361ca86f956a63ac795c6531fa7
Brandon-Blackstar <mor.bra34@gmail.com>
2023-02-27 12:15:35 +0100

    fix client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,21 +1,34 @@
 use std::env;
 use std::net::{TcpStream, TcpListener};
 use std::io::{BufReader, BufWriter, Write, Read};
+use serde_json;
 
 fn main() {
 
-    let server_address = "0.0.0.0:7878";
-    let mut stream = TcpStream::connect(server_address).unwrap();
-    let mut reader = BufReader::new(&stream);
-    let mut writer = BufWriter::new(&stream);
+    let stream = TcpStream::connect("127.0.0.1:7878");
+    match stream {
+        Ok(mut stream) => {
+            let message = "Hello";
+            let buf = message.as_bytes();
+            let n = buf.len() as u32;
+            let buf_n = n.to_be_bytes();
+            stream.write(&buf_n).unwrap();
+            stream.write(&buf).unwrap();
+        }
+        Err(err) => { panic!("ERROR: {err}") }
+    }
+
+
+    //let mut reader = BufReader::new(&stream);
+    //let mut writer = BufWriter::new(&stream);
 
     // Send Hello World to Server
-    let data = b"Hello, world!";
-    writer.write(data).unwrap();
-    writer.flush().unwrap();
+    //let data = b"Hello";
+    //writer.write(data).unwrap();
+    //writer.flush().unwrap();
 
     // Listen res from server
-    let mut response = Vec::new();
-    reader.read_to_end(&mut response).unwrap();
-    println!("Response from server: {:?}", response);
+    //let mut response = Vec::new();
+    //reader.read_to_end(&mut response).unwrap();
+    //println!("Response from server: {:?}", response);
 }
